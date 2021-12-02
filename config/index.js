139ccb406eb8764ad616ae55a830906f1d9d1115
camilomontoyau <camilo.montoya@techstars.com>
2021-12-02 17:25:05 -0500

    dbOptions

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ module.exports = {
   "development": null,
   "test": {
     uri: 'postgres://test_user:test_pwd@localhost:5432/test_db',
-    dbOptions: {},
+    dbOptions: {
+      define: {
+        timestamps: true,
+        paranoid: true
+      }
+    },
     server: {
       port: process.env.PORT || 3000
     }
