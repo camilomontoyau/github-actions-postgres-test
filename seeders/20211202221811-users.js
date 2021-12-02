963ea374ec67fad1c57490b5625da1bd43f6f4c7
camilomontoyau <camilo.montoya@techstars.com>
2021-12-02 17:45:13 -0500

    missing timestamps at seeder

--- seeders/20211202221811-users.js
+++ seeders/20211202221811-users.js
@@ -2,14 +2,16 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('users', [{
+    await queryInterface.bulkInsert('Users', [{
       firstName: 'John',
       lastName: 'Doe',
-      email: 'john.doe@example.com'
+      email: 'john.doe@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
